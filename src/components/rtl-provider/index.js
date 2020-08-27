64555225e8cfc243f3a36c5c861836996f05c0cc
sohaib <sohaib.code@gmail.com>
2020-08-27 19:23:23 +0100

    rtl support

---
+++ src/components/rtl-provider/index.js
@@ -0,0 +1,23 @@
+// import React, { useEffect } from 'react'
+
+// export default function RTLProvider({rtl, {children}}) {
+//     useEffect(() => {
+//        if (rtl) {
+//            document.querySelector('body').setAttribute('dir','rtl')
+//        }
+//     }, [rtl])
+//     if(rtl) {
+//         return (
+//             <div >
+
+//             </div>
+//         )
+
+//     }
+//             return (
+//                 <div >
+
+//                 </div>
+//             )
+
+// }
--- src/theme/index.js
+++ src/theme/index.js
@@ -4,8 +4,12 @@ import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { useSelector } from 'react-redux';
 
+import { create } from 'jss';
+import rtl from 'jss-rtl';
+import { StylesProvider, jssPreset } from '@material-ui/core/styles';
+
 export const Theme = ({ children }) => {
-  const darkMode = useSelector((state) => state.layout.darkMode);
+  const { darkMode, language } = useSelector((state) => state.layout);
   const darkTheme = {
     palette: {
       type: 'dark',
@@ -19,6 +23,23 @@ export const Theme = ({ children }) => {
   };
 
   const theme = createMuiTheme(darkMode === 'on' ? darkTheme : lightTheme);
+
+  if (language === 'ar') {
+    theme.direction = 'rtl';
+    document.querySelector('body').setAttribute('dir', 'rtl');
+    // Configure JSS
+    const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
+    return (
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <StylesProvider jss={jss}>{children}</StylesProvider>;
+      </ThemeProvider>
+    );
+  } else {
+    document.querySelector('body').setAttribute('dir', 'ltr');
+    theme.direction = 'ltr';
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
